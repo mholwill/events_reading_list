d7680abec6d8c0cb9c5e46fd3234418fb19c77b7
mholwill <m_holwill@hotmail.co.uk>
2020-02-20 15:50:26 +0000

    Form completed

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -2,17 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleSubmit);
+  // const formSelect = document.querySelector('#category');
+  // formSelect.addEventListener('select', handleSelect);
 
 
 })
 
 const handleSubmit = function() {
-  const list = document.querySelector('#reading-list')
-
+  const list = document.querySelector('#reading-list');
   const newListItem = document.createElement('li');
+  const titleParagraph = document.createElement('p');
+  const authorParagraph = document.createElement('p');
+  const categoryParagraph = document.createElement('p');
 
   event.preventDefault();
-  // console.log(resultParagraph);
-  newListItem.textContent = `Title: ${event.target.title.value}`;
+
+  titleParagraph.textContent = `Title: ${event.target.title.value}`;
+  newListItem.appendChild(titleParagraph);
+  authorParagraph.textContent = `Author: ${event.target.author.value}`;
+  newListItem.appendChild(authorParagraph);
+  categoryParagraph.textContent = `Category: ${event.target.category.value}`;
+  newListItem.appendChild(categoryParagraph);
+
   list.appendChild(newListItem);
 };
+
+// const handleSelect = function() {
+  // const categoryParagraph = document.createElement('p');
+  // categoryParagraph.textContent = `Author: ${event.target.category.value}`;
+  // newListItem.appendChild(categoryParagraph);
+  // console.log(handleSelect);
+// };
